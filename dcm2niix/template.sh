643ff957975dc53d48a81579e3e3eb77d0906b58
Muhammad Umer Khan <mukhan@gra-login2.graham.sharcnet>
2019-09-28 19:22:27 -0400

    comments added to explain code

--- dcm2niix/template.sh
+++ dcm2niix/template.sh
@@ -10,8 +10,19 @@ echo "Running on hostname `hostname`"
 
 echo "Starting run at: `date`"
 
-#  -m y => merge 2D slices from same series regardless of study time, echo, coil, orientation, etc. (y/n, default n)
-#  -f (%n_%p_%s) => patientName_protocol_seriesNumber
+
+# definitions
+#### $DCM2NIIXROOT - directory of dcm2niix installation
+#### $PROJECTDIR - directory of the project containing subject folders
+#### $SUBJECTDIR - specific subject directory containing unsorted/sorted DCM files
+
+#### Note: $PROJECTDIR + $SUBJECTDIR define the directory that should be sorted. In my template, a project folder will contain a list of subject folders.
+#### That is why the template is structured as above, but can be modified as desired.
+
+# dcm2niix specific flags
+#### -m y => merge 2D slices from same series regardless of study time, echo, coil, orientation, etc. (y/n, default n)
+####  -f (%n_%p_%s) => nifti filename => patientName_protocol_seriesNumber ; for a full specification of dicom header tags, refer to `man dcm2niix` or https://www.dicomlibrary.com/dicom/dicom-tags/
+
 $DCM2NIIXROOT $PROJECTDIR/$SUBJECTDIR -m y -f %n_%p_%s $PROJECTDIR/$SUBJECTDIR
 
 wait
